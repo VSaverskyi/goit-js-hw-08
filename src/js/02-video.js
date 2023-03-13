be5762bd22940037dfaf7b592cb6fda196d71415
VSaverskyi <vovikck15@gmail.com>
2023-03-13 20:08:27 +0100

    task 2 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,29 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+const onTimeupdate = function ({ seconds }) {
+  localStorage.setItem('videoplayer-current-time', JSON.stringify(seconds));
+};
+const getCurrentTime = function () {
+  return JSON.parse(localStorage.getItem('videoplayer-current-time'));
+};
+const throttledOnTimeupdate = throttle(onTimeupdate, 1000);
+
+player
+  .setCurrentTime(getCurrentTime())
+  .then(function (seconds) {})
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        'the time was less than 0 or greater than the video’s duration';
+        break;
+
+      default:
+        'some other error occurred';
+        break;
+    }
+  });
+player.on('timeupdate', throttledOnTimeupdate);
